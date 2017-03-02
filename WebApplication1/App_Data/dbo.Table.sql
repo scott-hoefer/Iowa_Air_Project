c4dbcfc539f8cf0b98e8338983ea15081919fe43
scott-hoefer <scott-hoefer@uiowa.edu>
2017-03-01 20:45:34 -0600

    Log in for generic user is working.

--- WebApplication1/App_Data/dbo.Table.sql
+++ WebApplication1/App_Data/dbo.Table.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Table]
 (
 	[accountNumber] INT NOT NULL PRIMARY KEY, 
-    [username] NCHAR(10) NULL, 
-	[email address] NCHAR(10) NULL,
-    [password] NCHAR(10) NULL, 
+    [username] NVARCHAR(50) NULL, 
+	[email address] NVARCHAR(100) NULL,
+    [password] NVARCHAR(100) NULL, 
     [account type] NCHAR(10) NULL DEFAULT user, 
     [confirmed] BIT NULL DEFAULT 0, 
     
