02dc68495712e20b9d4338f16d5e93cd7ed5608f
scott-hoefer <scott-hoefer@uiowa.edu>
2017-04-29 16:42:21 -0500

    Booking works with email confirmation of tickets

--- WebApplication1/EnterBookingInfo.aspx.cs
+++ WebApplication1/EnterBookingInfo.aspx.cs
@@ -4,6 +4,8 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Mail;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -132,6 +134,8 @@ namespace WebApplication1
                     addToDB(tb, dob);
                 }
             }
+            SendMail();
+            Response.Redirect("BookingComplete.aspx");
         }
 
         // add to database
@@ -149,6 +153,68 @@ namespace WebApplication1
             cmd.CommandText = "insert into Reservations values('" + id + "' , '" + TB.Text + "' , '" + dob + "' , '" + depart + "' , '" + returnDate + "' , '" + DBNull.Value + "' , '" + DBNull.Value + "' , '" + DBNull.Value + "' , '" + rid + "' , '" + seatType + "')";
             cmd.ExecuteNonQuery();
             con.Close();
+        }
+
+        public void SendMail()
+        {
+            string pGmailEmail = "iowaair06A@gmail.com";
+            string pGmailPassword = "Pword1234!";
+            string pTo = User.Identity.Name; //abc@domain.com
+            string pSubject = "Thanks For Your Purchase";
+            //string pBody = "Please click " + '<a runat='server' href='localhost:49384/EmailConfirmed'>Here</a>' + " to confirm your email."; //Body
+            MailFormat pFormat = MailFormat.Html; //Text Message
+            string pAttachmentPath = string.Empty; //No Attachments
+
+            System.Web.Mail.MailMessage myMail = new System.Web.Mail.MailMessage();
+            myMail.Fields.Add
+                ("http://schemas.microsoft.com/cdo/configuration/smtpserver",
+                              "smtp.gmail.com");
+            myMail.Fields.Add
+                ("http://schemas.microsoft.com/cdo/configuration/smtpserverport",
+                              "465");
+            myMail.Fields.Add
+                ("http://schemas.microsoft.com/cdo/configuration/sendusing",
+                              "2");
+            //sendusing: cdoSendUsingPort, value 2, for sending the message using 
+            //the network.
+
+            //smtpauthenticate: Specifies the mechanism used when authenticating 
+            //to an SMTP 
+            //service over the network. Possible values are:
+            //- cdoAnonymous, value 0. Do not authenticate.
+            //- cdoBasic, value 1. Use basic clear-text authentication. 
+            //When using this option you have to provide the user name and password 
+            //through the sendusername and sendpassword fields.
+            //- cdoNTLM, value 2. The current process security context is used to 
+            // authenticate with the service.
+            myMail.Fields.Add
+            ("http://schemas.microsoft.com/cdo/configuration/smtpauthenticate", "1");
+            //Use 0 for anonymous
+            myMail.Fields.Add
+            ("http://schemas.microsoft.com/cdo/configuration/sendusername",
+                pGmailEmail);
+            myMail.Fields.Add
+            ("http://schemas.microsoft.com/cdo/configuration/sendpassword",
+                 pGmailPassword);
+            myMail.Fields.Add
+            ("http://schemas.microsoft.com/cdo/configuration/smtpusessl",
+                 "true");
+            myMail.From = pGmailEmail;
+            myMail.To = pTo;
+            myMail.Subject = pSubject;
+            myMail.BodyFormat = pFormat;
+            //myMail.Body = "Please click <a href=\"http://localhost:49384/EmailConfirmed.aspx\">here</a> to confirm your email.";
+            myMail.Body = "Thanks for choosing to fly Air Iowa! Please log onto your account to check flight details.";
+            if (pAttachmentPath.Trim() != "")
+            {
+                MailAttachment MyAttachment =
+                        new MailAttachment(pAttachmentPath);
+                myMail.Attachments.Add(MyAttachment);
+                myMail.Priority = System.Web.Mail.MailPriority.High;
+            }
+
+            SmtpMail.SmtpServer = "smtp.gmail.com:465";
+            SmtpMail.Send(myMail);
         }
     }
 }
