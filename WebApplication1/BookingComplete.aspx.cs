b60c7d78bad3d3976d6f31c849d09004aa037d97
scott-hoefer <scott-hoefer@uiowa.edu>
2017-04-29 19:05:18 -0500

    trying to recover

--- WebApplication1/BookingComplete.aspx.cs
+++ WebApplication1/BookingComplete.aspx.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace WebApplication1
 {
     public partial class BookingComplete : System.Web.UI.Page
     {
+        int numPass;
+        SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=aspnet-WebApplication1-20170209101639;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        SqlCommand cmd;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            int numPass = Convert.ToInt16(Session["numPassengers"]);
         }
+
+
     }
 }
